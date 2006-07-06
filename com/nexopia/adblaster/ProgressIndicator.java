d1368379ac9c68e7312da2e762a85e965c709173
thomas <thomas@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-07-06 23:31:06 +0000

    Added a file.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@3138 7be605d3-0650-4ba7-bb8c-0af3a60b5258

---
+++ com/nexopia/adblaster/ProgressIndicator.java
@@ -0,0 +1,32 @@
+package com.nexopia.adblaster;
+
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+
+public class ProgressIndicator {
+	static JProgressBar bar;
+	
+	static {
+		bar = new JProgressBar(0, 100);
+		bar.setValue(0);
+		bar.setStringPainted(true);
+		JFrame frame = new JFrame();
+		frame.setContentPane(bar);
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	public static void show(int i, int max){
+		bar.setMaximum(max);
+		bar.setValue(i);
+	}
+	
+	public static void main(String args[]){
+		int i = 0;
+		while(true){
+			i++;
+			show(i, 10000000);
+		}
+	}
+
+}
