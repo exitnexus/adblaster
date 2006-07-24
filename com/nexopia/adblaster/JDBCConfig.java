c7f20a8d166894cd1cec32773cda9bb4af13dc6f
baxter <baxter@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-07-24 20:20:58 +0000

    Pulled the database connection information into a static class so it can be all changed in one place.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@3672 7be605d3-0650-4ba7-bb8c-0af3a60b5258

---
+++ com/nexopia/adblaster/JDBCConfig.java
@@ -0,0 +1,29 @@
+package com.nexopia.adblaster;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCConfig {
+	private static Connection con; 
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://192.168.0.50:3307/banner";
+			con = DriverManager.getConnection(url, "nathan", "nathan");
+		} catch (ClassNotFoundException e) {
+			System.err.println("Unable to load JDBC driver.");
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (SQLException e) {
+			System.err.println("Unable to establish connection to banner database.");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	
+	public static Statement createStatement() throws SQLException {
+		return con.createStatement();
+	}
+}
