aba1399c323d19ba1889cf00ed8dd5ffa9a0b116
baxter <baxter@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-09-09 00:09:30 +0000

    Added an SQL worker thread class, just sits on a queue waiting for a query string and when one is there it executes it.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@4647 7be605d3-0650-4ba7-bb8c-0af3a60b5258

---
+++ com/nexopia/adblaster/SQLQueue.java
@@ -0,0 +1,66 @@
+package com.nexopia.adblaster;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+
+public class SQLQueue
+{
+	private final int nThreads;
+	private final SQLWorker[] threads;
+	private final LinkedList<String> queue;
+	
+	public SQLQueue(int nThreads)
+	{
+		this.nThreads = nThreads;
+		queue = new LinkedList<String>();
+		threads = new SQLWorker[nThreads];
+		
+		for (int i=0; i<nThreads; i++) {
+			threads[i] = new SQLWorker();
+			threads[i].start();
+		}
+	}
+	
+	public void execute(String query) {
+		synchronized(queue) {
+			queue.addLast(query);
+			queue.notify();
+		}
+	}
+	
+	private class SQLWorker extends Thread {
+		public void run() {
+			String query;
+			
+			while (true) {
+				synchronized(queue) {
+					while (queue.isEmpty()) {
+						try
+						{
+							queue.wait();
+						}
+						catch (InterruptedException ignored)
+						{
+						}
+					}
+					
+					query =  queue.removeFirst();
+				}
+				
+				try {
+					Statement st = JDBCConfig.createStatement();
+					st.execute(query);
+				}
+				catch (RuntimeException e) {
+					// If we don't catch RuntimeException, 
+					// the pool could leak threads
+					System.err.println("Runtime exception while executing query '" + query + "'.");
+				} catch (SQLException e) {
+					System.err.println("Error executing query '" + query + "'.");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
