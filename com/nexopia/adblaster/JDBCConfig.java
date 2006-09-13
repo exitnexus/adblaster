a6e06524fe66c627ecd01432de68e6db5b4d46ee
thomas <thomas@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-09-13 22:43:24 +0000

    Change which database we access.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@4737 7be605d3-0650-4ba7-bb8c-0af3a60b5258

--- com/nexopia/adblaster/JDBCConfig.java
+++ com/nexopia/adblaster/JDBCConfig.java
@@ -6,18 +6,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JDBCConfig {
-	public static final String BANNERSTAT_TABLE = "testbannerstats";	
-	public static final String BANNERTYPESTAT_TABLE = "testbannertypestats";
-	public static final String BANNER_TABLE = "testbanners";
-	public static final String CAMPAIGN_TABLE = "testbannercampaigns";
+	public static final String BANNERSTAT_TABLE = "bannerstats";	
+	public static final String BANNERTYPESTAT_TABLE = "bannertypestats";
+	public static final String BANNER_TABLE = "banners";
+	public static final String CAMPAIGN_TABLE = "bannercampaigns";
 
 	private static Connection con; 
 	private static SQLQueue sqlQueue;
 	static {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://192.168.0.50:3307/banner";
-			con = DriverManager.getConnection(url, "nathan", "nathan");
+			String url = "jdbc:mysql://192.168.0.150:3307/testbanner";
+			con = DriverManager.getConnection(url, "root", "Hawaii");
 			sqlQueue = new SQLQueue(1);
 		} catch (ClassNotFoundException e) {
 			System.err.println("Unable to load JDBC driver.");
