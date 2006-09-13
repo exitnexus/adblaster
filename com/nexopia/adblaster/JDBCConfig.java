605eb4b30131c7d6528b23557ab080af9f4a1d79
baxter <baxter@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-09-13 23:04:57 +0000

    fixed database address
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@4738 7be605d3-0650-4ba7-bb8c-0af3a60b5258

--- com/nexopia/adblaster/JDBCConfig.java
+++ com/nexopia/adblaster/JDBCConfig.java
@@ -16,7 +16,7 @@ public class JDBCConfig {
 	static {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://192.168.0.150:3307/testbanner";
+			String url = "jdbc:mysql://192.168.0.50:3306/testbanner";
 			con = DriverManager.getConnection(url, "root", "Hawaii");
 			sqlQueue = new SQLQueue(1);
 		} catch (ClassNotFoundException e) {
