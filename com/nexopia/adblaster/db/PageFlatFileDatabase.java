7298dc2edbb467a02e7033422f872ad18bda59ce
baxter <baxter@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-12-04 23:20:29 +0000

    Added a page flat file database.  This one encompases both reading and writing and keeps everything in memory.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@6380 7be605d3-0650-4ba7-bb8c-0af3a60b5258

---
+++ com/nexopia/adblaster/db/PageFlatFileDatabase.java
@@ -0,0 +1,68 @@
+package com.nexopia.adblaster.db;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class PageFlatFileDatabase {
+	File file;
+	File directory;
+	FileWriter writer;
+	BufferedReader reader;
+	
+	private HashMap<String, Integer> pages; //track the pages in the database
+	private int count;
+	
+	public PageFlatFileDatabase(String directoryName, boolean append) throws IOException {
+		this.init(directoryName, append);
+	}
+	
+	private void init(String directoryName, boolean append) throws IOException {
+		directory = new File(directoryName);
+		if (!directory.isDirectory()) {
+			directory.mkdir();
+			if (!directory.isDirectory()) {
+				throw new SecurityException(directoryName + " is not a directory and cannot be created as one.");
+			}
+		}
+		file = new File("page.db");
+		writer = new FileWriter(file, append);
+		reader = new BufferedReader(new FileReader(file));
+		if (append) {
+			this.load();
+		} else {
+			pages = new HashMap<String, Integer>();
+		}
+	}
+	
+	public void load() throws IOException {
+		pages = new HashMap<String, Integer>();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			String words[] = line.split(" ");
+			pages.put(words[1], Integer.valueOf(words[0]));
+		}
+	}
+	
+	//returns the id assigned to the page
+	public int write(String page) throws IOException {
+		if (!pages.containsKey(page)) {
+			pages.put(page, Integer.valueOf(count));
+			count++;
+		}
+		return pages.get(page).intValue();
+	}
+	
+	public void sync() throws IOException {
+		writer.flush();
+	}
+
+	public void close() throws IOException {
+		this.sync();
+		writer.close();
+	}
+	
+}
