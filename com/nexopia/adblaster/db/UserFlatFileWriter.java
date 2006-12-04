f37644930974f09446aaea08cea2899360acbac3
baxter <baxter@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-12-04 21:55:13 +0000

    Added a bitset to the user flat file writer so that it can track which users have already been inserted based on id.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@6375 7be605d3-0650-4ba7-bb8c-0af3a60b5258

--- com/nexopia/adblaster/db/UserFlatFileWriter.java
+++ com/nexopia/adblaster/db/UserFlatFileWriter.java
@@ -3,6 +3,7 @@ package com.nexopia.adblaster.db;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.BitSet;
 import java.util.Vector;
 
 import com.nexopia.adblaster.struct.User;
@@ -11,8 +12,18 @@ public class UserFlatFileWriter {
 	private static final int FILE_COUNT = 100; //How many files should the users be split across 
 	Vector<FileWriter> files;
 	File directory;
+	private BitSet users; //track the users that have been written.
+	
 	
 	public UserFlatFileWriter(String directoryName, boolean append) throws IOException {
+		this.init(directoryName, append, true);
+	}
+	
+	public UserFlatFileWriter(String directoryName, boolean append, boolean trackUsers) throws IOException {
+		this.init(directoryName, append, trackUsers);
+	}
+	
+	private void init(String directoryName, boolean append, boolean trackUsers) throws IOException {
 		files = new Vector<FileWriter>();
 		directory = new File(directoryName);
 		if (!directory.isDirectory()) {
@@ -25,11 +36,17 @@ public class UserFlatFileWriter {
 			File f = new File(directory, i+".user.db");
 			files.add(new FileWriter(f, append));
 		}
+		if (trackUsers) {
+			users = new BitSet();
+		}
 	}
 	
 	public void insert(User u) throws IOException {
-		int fileID = u.getID()%FILE_COUNT;
-		files.get(fileID).write(u.databaseString());
+		if (!users.get(u.getID())) {
+			int fileID = u.getID()%FILE_COUNT;
+			files.get(fileID).write(u.databaseString());
+			users.set(u.getID());
+		}
 	}
 	
 	public void sync() throws IOException {
