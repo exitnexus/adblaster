e6854706c09adf85f6c02368953c292f35621c02
thomas <thomas@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-12-05 01:34:07 +0000

    Added flat file banner db class.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@6388 7be605d3-0650-4ba7-bb8c-0af3a60b5258

---
+++ com/nexopia/adblaster/BannerViewFFDatabase.java
@@ -0,0 +1,42 @@
+package com.nexopia.adblaster;
+
+import java.io.File;
+import java.util.Vector;
+
+import com.nexopia.adblaster.db.BannerViewBinding;
+import com.nexopia.adblaster.struct.BannerView;
+
+public class BannerViewFFDatabase {
+
+	public int lastid;
+
+	public BannerViewFFDatabase(File f, BannerViewBinding instanceBinding) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BannerViewCursor getCursor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getBannerViewCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public BannerView get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Vector<BannerView> getByUser(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
