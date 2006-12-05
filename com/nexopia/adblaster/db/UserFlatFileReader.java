b3406cd105446846325ca5c4d827a39067e31738
baxter <baxter@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-12-05 02:04:44 +0000

    Fixed minor bugs related to the hashmap loading, now compiles.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@6393 7be605d3-0650-4ba7-bb8c-0af3a60b5258

--- com/nexopia/adblaster/db/UserFlatFileReader.java
+++ com/nexopia/adblaster/db/UserFlatFileReader.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Vector;
 
 import com.nexopia.adblaster.struct.User;
+import com.nexopia.adblaster.util.IntObjectHashMap;
 
 public class UserFlatFileReader {
 
@@ -32,10 +34,10 @@ public class UserFlatFileReader {
 		users = new IntObjectHashMap<User>();
 	}
 	
-	public void load(int fileNumber) throws FileNotFoundException {
+	public void load(int fileNumber) throws IOException {
 		files.get(fileNumber);
 		File f = new File(directory, "user."+fileNumber+".db");
-		reader = new BufferedReader(FileReader(f));
+		reader = new BufferedReader(new FileReader(f));
 		String userString;
 		while ((userString = reader.readLine()) != null) {
 			User u = new User(userString);
