435c0f43e9b916626782df88324f1d90cee77711
baxter <baxter@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-12-08 20:51:12 +0000

    Added stub file to start new adblaster revision.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@6459 7be605d3-0650-4ba7-bb8c-0af3a60b5258

---
+++ com/nexopia/adblaster/NeoAdBlaster.java
@@ -0,0 +1,13 @@
+package com.nexopia.adblaster;
+
+public class NeoAdBlaster {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
