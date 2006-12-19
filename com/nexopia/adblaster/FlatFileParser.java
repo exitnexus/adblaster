ab409e593aea990484913267d3a82b34e579408f
thomas <thomas@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2006-12-19 01:22:57 +0000

    Set up FlatFileParser to load FlatFile DBs.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@6581 7be605d3-0650-4ba7-bb8c-0af3a60b5258

--- com/nexopia/adblaster/FlatFileParser.java
+++ com/nexopia/adblaster/FlatFileParser.java
@@ -6,23 +6,54 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import com.nexopia.adblaster.db.BannerViewFlatFileReader;
+import com.nexopia.adblaster.db.UserFlatFileReader;
+import com.nexopia.adblaster.struct.BannerView;
+import com.nexopia.adblaster.struct.User;
+import com.nexopia.adblaster.util.Integer;
+
 public class FlatFileParser {
 	public File f;
-	BufferedReader bfr;
+	int current_file;
+	int current_index;
+	BannerViewFlatFileReader bvffr;
+	UserFlatFileReader uffr;
 	
 	FlatFileParser(String fname){
 		try {
-			f = new File(fname);
-			FileReader fr;
-			fr = new FileReader(f);
-			bfr = new BufferedReader(fr);
+			bvffr = new BannerViewFlatFileReader(fname);
+			uffr = new UserFlatFileReader(fname); 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		current_file = -1;
+		current_index = -1;
+		
+	}
+	
+	void loadNext(){
+		current_file++;
+		try {
+			bvffr.load(current_file);
+			uffr.load(current_file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		current_index = 0;
 	}
 	
 	String readLine() throws IOException{
-		return bfr.readLine();
+		if (current_index >= bvffr.getBannerViewCount()){
+			loadNext();
+		}
+		
+		BannerView bv = bvffr.getCurrentBannerViews().get(current_index);
+		User u = uffr.getUser(bv.getUserID());
+		
+		return "get " + bv.getTime() + " " + bv.getSize() + " " + bv.getUserID() + 
+			" " + u.getAge() + " " + u.getSex() + " " + u.getLocation() + " " + 
+			u.getInterests()+ " " + bv.getPage() + " " + bv.getPassback() + " " + "0\n";  
 	}
 }
