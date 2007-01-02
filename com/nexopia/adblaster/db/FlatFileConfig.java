4d1416237d6640eb1cb1c4c33a0b3867856ba7c2
baxter <baxter@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2007-01-02 23:09:37 +0000

    Added code to look for a flat file default directory, starts at yesterday and works backwards until it hits today.  Raises an exception if it finds no valid directory.
    git-svn-id: file:///var/svn/nexopia/trunk/adblaster@6628 7be605d3-0650-4ba7-bb8c-0af3a60b5258

--- com/nexopia/adblaster/db/FlatFileConfig.java
+++ com/nexopia/adblaster/db/FlatFileConfig.java
@@ -1,5 +1,30 @@
 package com.nexopia.adblaster.db;
 
+import java.io.File;
+import java.util.Calendar;
+
 public class FlatFileConfig {
 	public static final int FILE_COUNT = 100;
+	
+	public static File getDefaultDirectory() {
+		int day = dayBefore(Calendar.DAY_OF_YEAR); 
+		File directory = null;
+		
+		while ((directory != null || !directory.canRead()) && day != Calendar.DAY_OF_YEAR) {
+			directory = new File("DB_" + day);
+		}
+		
+		if (!directory.canRead()) {
+			throw new RuntimeException("Unable to find any valid directories for default flat file directory.");
+		}
+		return directory;			
+	}
+	
+	private static int dayBefore(int day) {
+		if (day <= 0) {
+			return 366;
+		} else {
+			return day-1;
+		}
+	}
 }
